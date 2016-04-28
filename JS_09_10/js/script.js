b4fa5c88775267cc2396531dba0d263c405b1860
Dmitriy Vepretskiy <lynch-masta@mail.ru>
2016-04-28 20:45:11 +0300

    In HW_JS_9-10 fixed javascript animation (bonus task)

--- JS_09_10/js/script.js
+++ JS_09_10/js/script.js
@@ -65,28 +65,102 @@ $(function() {
     });
 });
 /* Dropdown menu with JavaScript only */
+
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('DOM is loaded');
-  var dropdown = document.querySelectorAll('.menu.javaScript .dropdown');
-  /* Должна быть функция анимации slideUp (фактическая высота элемента кэшируется,
-текущая ставится в ноль и через setInterval плавно увеличиваем высоту до
-фактической)
-Проблема в том, что hover срабатывает не только на ul но и на каждом li в нем.
-Если отменить всплытие, то при уходе курсора с первого li происходит mouseout
-и ul исчезает*/
-  function slideUp(el) {
-    var elHeight = getComputedStyle(el).height;
-    console.log('element ', el, ' height=',elHeight);
-  }
-  /* Конец функции slideUp */
+
+  var menu = document.querySelector('.menu.javaScript');
+  var dropdown = menu.querySelectorAll('.dropdown');
+  var submenu = menu.querySelectorAll('.submenu');
+// Добавляем атрибут уровня для определения направления движения
+  dropdown[0].setAttribute('data-level', 0);
   for (var i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener('mouseover', function(e) {
-      this.querySelector('.submenu').style.display = "block";
-      /* Отправляем  на анимацию вложенный ul.submenu*/
-      slideUp(this.querySelector('.submenu'));
-    });
-    dropdown[i].addEventListener('mouseout', function(e) {
-      this.querySelector('.submenu').style.display = "none";
-    });
+    submenu[i].setAttribute('data-level', i+1);
+    var submenuChildren = dropdown[i].querySelectorAll('li');
+    for (var x = 0; x < submenuChildren.length; x++) {
+      submenuChildren[x].setAttribute('data-level', i+1);
+    }
   }
-})
+  // Ловим событие mouseover над а внутри li.dropdown. Показываем меню - правого
+  // соседа submenu
+  var parentInTo, parentInFrom, childMenuIn, levelInTo, levelInFrom;
+  menu.addEventListener('mouseover', function(e) {
+    parentInTo = e.target.parentNode;
+    parentInFrom = e.relatedTarget.parentNode;
+    if ((parentInTo.className === 'dropdown')) {
+      childMenuIn = e.target.nextElementSibling;
+      childMenuIn.style.display = "block";
+      slideUp(childMenuIn);
+    }
+  });
+// Ловим событые mouseout при переходе с большего levela на меньший и
+// при выходе из тела меню
+  var parentOutTo, parentOutFrom, levelOutTo, levelOutFrom;
+  menu.addEventListener('mouseout', function(e) {
+    parentOutTo = e.relatedTarget.parentNode;
+    levelOutTo = parentOutTo.getAttribute('data-level');
+    parentOutFrom = e.target.parentNode;
+    levelOutFrom = parentOutFrom.getAttribute('data-level');
+    // Прячем при выходе из тела меню
+    if ((levelOutTo === null) && (levelOutTo !== levelOutFrom)) {
+      for (var y = 0; y < submenu.length; y++) {
+        submenu[y].style.display = "none";
+        submenu[y].style.height = "auto";
+      }
+    }
+    // Прячем меню-правого соседа при уходе курсора с dropdown левого соседа
+    if (parentOutFrom.className === 'dropdown' && parentOutTo.className !== 'dropdown' && levelOutFrom === levelOutTo && parentOutTo.className !== 'submenu') {
+      slideDown(e.target.nextElementSibling);
+    }
+    // Прячем при при переходе с большего уровня наменьший
+    if ((levelOutTo < levelOutFrom) && (levelOutTo !== null)) {
+      if (e.target.parentNode.className === 'submenu') {
+        slideDown(e.target.parentNode);
+        for (var z = levelOutTo; z < submenu.length; z++) {
+          submenu[z].style.display = "none";
+          submenu[z].style.height = "auto";
+        }
+      }
+    }
+  });
+
+  // Блок анимации slideUp / slideDown
+var duration = 300;
+var fps = 15;
+var frameRate = duration / fps;
+var oldHeight, height, step;
+
+  function slideUp(el) {
+    oldHeight = parseInt(getComputedStyle(el).height);
+    el.style.height = 0;
+    height = 0;
+    step = oldHeight / 20;
+    el.style.overflow = 'hidden';
+    var timer = setInterval (function(){
+      el.style.height = height + 'px';
+      if (height >= oldHeight) {
+        clearInterval(timer);
+        el.style.overflow = 'visible';
+        el.style.height = 'auto';
+      } else {
+        height = Math.round((height + step) * 100) / 100;
+      }
+    }, frameRate);
+  }
+
+  function slideDown(el) {
+    height = parseInt(getComputedStyle(el).height);
+    step = height / 20;
+    el.style.overflow = 'hidden';
+    var timer = setInterval (function(){
+      el.style.height = height + 'px';
+      if (height === 0 || height < 0) {
+        el.style.overflow = 'visible';
+        el.style.display = "none";
+        el.style.height = 'auto';
+        clearInterval(timer);
+      } else {
+        height = Math.round((height - step) * 100) / 100;
+      }
+    }, frameRate);
+  }
+});
